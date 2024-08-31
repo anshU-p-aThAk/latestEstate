c624fb1cc3be1e52cf7b1b98edc69405c1340470
Anshu Pathak <surajshiv15ss@gmail.com>
2024-08-31 14:06:21 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -64,3 +64,54 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] }; //In other words, if offer is undefined, the filter will not exclude any documents based on the offer field, and all documents will be retrieved.
+    } // that means offer can be true or false
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      //$option: "i" means not case-sensitive
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({
+        [sort]: order,
+      })
+      .limit(limit)
+      .skip(startIndex); //regex ->to perform pattern matching within strings. This can be particularly useful when you want to find documents where a certain field contains a string that matches a specific pattern.
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};

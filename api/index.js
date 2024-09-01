edfc5e4251321fc55e5dd57272a454e2b2348288
Anshu Pathak <surajshiv15ss@gmail.com>
2024-09-01 18:15:09 +0530

    ready to deploy

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import listingRouter from "./routes/listing.route.js";
-
+import path from "path";
 const app = express();
 app.use(express.json());
 dotenv.config();
@@ -20,10 +20,17 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 //Error middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
